fabd768e8765124069b3c334aa185dc560cac26b
Guillem Calabuig Domenech <guillermoaoe@gmail.com>
2022-03-10 14:51:09 +0100

    minsky

---
+++ examples/minsky-viewer.config.js
@@ -0,0 +1,34 @@
+(function() {
+    "use strict";
+    
+    // Custom configuration for the Volume Viewer demo app.
+    BrainBrowser.config.set("color_maps", [
+      {
+        name: "Spectral",
+        url: "color-maps/spectral-brainview.txt",
+        cursor_color: "#FFFFFF"
+      },
+      {
+        name: "Thermal",
+        url: "color-maps/thermal.txt",
+        cursor_color: "#FFFFFF"
+      },
+      {
+        name: "Gray",
+        url: "color-maps/gray-scale.txt",
+        cursor_color: "#FF0000"
+      },
+      {
+        name: "Blue",
+        url: "color-maps/blue.txt",
+        cursor_color: "#FFFFFF"
+      },
+      {
+        name: "Green",
+        url: "color-maps/green.txt",
+        cursor_color: "#FF0000"
+      }
+    ]);
+      
+  })();
+  
---
+++ examples/minsky-viewer.js
@@ -0,0 +1,66 @@
+$(function () {
+	"use strict";
+
+	$(".button").button();
+
+	//Inicio del viewer
+	BrainBrowser.VolumeViewer.start(
+		"brainbrowser",
+		function (viewer) {	 
+			
+			// Set the size of slice display panels.
+			$("#brainbrowser-wrapper").css("width", "90%");
+			$("#volume-viewer").css("width", "100%");
+			$("#brainbrowser").css("width", "100%");
+			
+			// Tamaño inicial por defecto (un medio de la altura de la ventana)
+			var height = window.innerHeight / 2.1;
+			viewer.setDefaultPanelSize(height, height);
+
+			// Load the default color map.
+			// (Second argument is the cursor color to use).
+			var color_map_config = BrainBrowser.config.get("color_maps")[0];
+			viewer.loadDefaultColorMapFromURL(color_map_config.url, "#FF0000");
+			
+			// Event listener para resize de la ventana.
+			window.addEventListener('resize', () => {
+				height = window.innerHeight / 2.1;
+				viewer.setPanelSize(height, height, { scale_image: true });
+			}, false);
+
+			// Event listener para cuando ha cargado.
+			viewer.addEventListener("volumesloaded", function () {
+				console.log("Viewer is ready!");
+				});
+
+			// Renderiza el viewer.
+			viewer.render();
+			
+			// Carga las imagenes
+			viewer.loadVolumes({
+				volumes: [
+				{
+				type: "minc",
+				header_url: "models/structural1.mnc.header",
+				raw_data_url: "models/structural1.mnc.raw",
+				template: {
+					element_id: "volume-ui-template",
+					viewer_insert_class: "volume-viewer-display",
+				},
+				},
+			],
+			overlay: {
+				template: {
+				element_id: "overlay-ui-template",
+				viewer_insert_class: "overlay-viewer-display",
+				},
+			},
+			complete: function () {
+				//loading_div.hide();
+				$("#brainbrowser-wrapper").slideDown({ duration: 600 });
+			},
+			});
+
+		}
+	);
+});
