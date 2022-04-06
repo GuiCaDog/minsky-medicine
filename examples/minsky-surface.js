a0fadee5759e8c71069744c2efff80df4838d19d
Jorpeser <pepeperezvalenzuela@gmail.com>
2022-04-06 18:14:40 +0200

    Código comentado y limpieza de archivos-codigo

--- examples/minsky-surface.js
+++ examples/minsky-surface.js
@@ -1,106 +1,34 @@
-/*
-* BrainBrowser: Web-based Neurological Visualization Tools
-* (https://brainbrowser.cbrain.mcgill.ca)
-*
-* Copyright (C) 2011
-* The Royal Institution for the Advancement of Learning
-* McGill University
-*
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU Affero General Public License as
-* published by the Free Software Foundation, either version 3 of the
-* License, or (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU Affero General Public License for more details.
-*
-* You should have received a copy of the GNU Affero General Public License
-* along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
-/*
-* Author: Tarek Sherif <tsherif@gmail.com> (http://tareksherif.ca/)
-* Author: Nicolas Kassis
-*/
-
-// This script is meant to be a demonstration of how to
-// use most of the functionality available in the
-// BrainBrowser Surface Viewer.
-$(function() {
+$(function () {
   "use strict";
 
-  var THREE = BrainBrowser.SurfaceViewer.THREE;
-  var atlas_labels = {};
-
-  // Request variables used to cancel the current request
-  // if another request is started.
-  var current_request = 0;
-  var current_request_name = "";
-
-  // Hide or display loading icon.
-  var loading_div = $("#loading");
-  function showLoading() { loading_div.show(); }
-  function hideLoading() { loading_div.hide(); }
-
-
-  // Make sure WebGL is available.
+  // Nos aseguramos que WEBGL esté habilitado.
   if (!BrainBrowser.WEBGL_ENABLED) {
     $("#brainbrowser").html(BrainBrowser.utils.webGLErrorMessage());
     return;
   }
 
-  $.get("models/atlas-labels.txt", function(data) {
-    var lines = data.split("\n");
-    var regex = /'(.+)'\s+(\d+)/;
+  // Iniciamos el visor de superficie.
+  BrainBrowser.SurfaceViewer.start(
+    "brainbrowser1", // id del div del HTML donde se insertará el viewer.
+    function (viewer) {
 
-    lines.forEach(function(line) {
-      var match = line.match(regex);
-      if (match) {
-        atlas_labels[match[2]] = match[1];
-      }
-    });
-  });
-
-  /////////////////////////////////////
-  // Start running the Surface Viewer
-  /////////////////////////////////////
-	BrainBrowser.SurfaceViewer.start("brainbrowser1", function(viewer) {
-   
-  
-    //Add an event listener.
-    viewer.addEventListener("displaymodel", function() {
-      console.log("We have a model!");
-    });
-
-    // Start rendering the scene.
+    // Renderiza el viewer.
     viewer.render();
 
-    // Load a model into the scene.
-    //viewer.loadModelFromURL("/models/brain_surface.obj");
-    viewer.annotations.setMarkerRadius(1);
+    // Cargamos el modelo.
+    //viewer.annotations.setMarkerRadius(1);
     viewer.loadModelFromURL("models/brain-surface.obj", {
       format: "mniobj",
-      complete: function() {
+      complete: function () {
         $("#vertex-data-wrapper").show();
         $("#pick-value-wrapper").show();
         $("#pick-label-wrapper").show();
       },
-      parse: { split: true }
+      parse: { split: true },
     });
 
-    // Hook viewer behaviour into UI.
-    $("#wireframe").change(function(e) {
-      viewer.setWireframe($(this).is(":checked"));
-    });
-
-    $(window).resize(function() {
+    $(window).resize(function () {
       viewer.updateViewport();
     });
-    
-
-  }); 
-
+  });
 });
-
--- examples/minsky-viewer.js
+++ examples/minsky-viewer.js
@@ -3,28 +3,29 @@ $(function () {
 
 	$(".button").button();
 
-	//Inicio del viewer
+	// Inicio del volume viewer
 	BrainBrowser.VolumeViewer.start(
-		"brainbrowser",
+		"brainbrowser", // id del div del HTML donde se insertará el viewer.
 		function (viewer) {	 
 			
-			// Set the size of slice display panels.
-			$("#brainbrowser-wrapper").css("width", "90%");
+			// Tamaños de los contenedores del visor.
+			$("#brainbrowser-wrapper").css("width", "100%");
 			$("#volume-viewer").css("width", "100%");
 			$("#brainbrowser").css("width", "100%");
 			
-			
-			// Tamaño inicial por defecto (un medio de la altura de la ventana)
+			// Tamaño por defecto de los paneles 
+			// (algo menos un medio de la altura de la ventana)
 			var height = window.innerHeight / 2.1;
-			
 			viewer.setDefaultPanelSize(height, height);
 
-			// Load the default color map.
-			// (Second argument is the cursor color to use).
+			// Obtenemos el mapa de color del fichero de configuración del visor. (minsky-viewer.config.js)
+			// En este caso usaremos el mapa de color "Gray".
+			// Cargamos el mapa de color en el visor. El segundo argumento (#FFFFFF) es el color del cursor.
 			var color_map_config = BrainBrowser.config.get("color_maps")[2];
 			viewer.loadDefaultColorMapFromURL(color_map_config.url, "#FFFFFF");
-			//viewer.loadVolumeColorMap
-			// Event listener para resize de la ventana.
+
+			// Event listener para reescalar las imágenes al tamaño de la ventana.
+			// Cada vez que se redimensione la ventana, se recalcula el tamaño de los paneles.
 			window.addEventListener('resize', () => {
 				height = window.innerHeight / 2.1;
 				viewer.setPanelSize(height, height, { scale_image: true });
@@ -32,7 +33,7 @@ $(function () {
 
 			// Event listener para cuando ha cargado.
 			viewer.addEventListener("volumesloaded", function (event) {
-				var d = viewer.volumes[0].display;
+
 				var color_map = BrainBrowser.config.get("color_maps")[0];
         		viewer.loadVolumeColorMapFromURL(1, color_map.url, "#FF0000", function() {
         		  viewer.redrawVolumes();
@@ -44,19 +45,15 @@ $(function () {
 				console.log("Viewer is ready!");
 				$('#container-remove0').remove();
 				$('#container-remove1').remove();
-				});
+			});
 
 			// Renderiza el viewer.
 			viewer.render();
-			
-			// get url parameter called model
-			//var model = getUrlParameter('model');
-
+		
 			// Carga las imagenes
 			viewer.loadVolumes({
 				volumes: [
 				{
-				//type: "minc",
 				type: "nifti1",
 				//header_url: "models/structural1.mnc.header",
 				//raw_data_url: "models/structural1.mnc.raw",
@@ -68,7 +65,6 @@ $(function () {
 				},
 				},
 				{
-				//type: "minc",
 				type: "nifti1",
 				//header_url: "models/structural1.mnc.header",
 				//raw_data_url: "models/structural1.mnc.raw",
@@ -80,18 +76,12 @@ $(function () {
 				},
 				},
 			],
-
-			//Mirar como integrar overlay --- OVERLAY AQUI -----
 			overlay: {
 				template: {
 				element_id: "volume-ui-template",
 				viewer_insert_class: "volume-viewer-display",
 				},
-			},
-			complete: function () {
-				//loading_div.hide();
-				$("#brainbrowser-wrapper").slideDown({ duration: 600 });
-			},
+			}
 			});
 			
 		}
